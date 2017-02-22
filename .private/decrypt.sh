76792bcf143a279bea9593b01fd25b696c07c495
beardypig <beardypig@protonmail.com>
2017-02-22 16:16:36 +0000

    travis: don't decrypt the deploy key when it's a PR

---
+++ .private/decrypt.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Pull requests and commits to other branches shouldn't try to decrypt the private key
+if [ "$TRAVIS_PULL_REQUEST" != "false" -o "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
+    exit 0
+fi
+
+openssl aes-256-cbc -K $encrypted_c7d371b9a595_key -iv $encrypted_c7d371b9a595_iv -in .private/deploy_key.enc -out .private/deploy_key -d
